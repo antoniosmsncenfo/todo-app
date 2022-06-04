885ae525b4f8215032e8e47f51b56e911199869e
Manuel Alvarado <antoniosmsn@hotmail.com>
2022-06-04 13:06:57 -0600

    Limpia el proyecto de lo default y crea la ventana principal

---
+++ src/screens/MainAppScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+export const MainAppScreen = () => {
+  return (
+    <View>
+      <Text>Title</Text>
+    </View>
+  );
+};
