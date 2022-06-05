dd4bb8561eeda8d57781412eb988534ecc4d4b00
Manuel Alvarado <antoniosmsn@hotmail.com>
2022-06-04 21:32:03 -0600

    Limpia los imports que no se requieren

--- src/components/TaskComponent.tsx
+++ src/components/TaskComponent.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { string } from 'yargs';
 
 interface Props {
